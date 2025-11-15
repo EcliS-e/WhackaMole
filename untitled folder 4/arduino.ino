5ed7a2d106f5692d68d92999a9435593523eec65
Mariana Espinal <mce1571@g.rit.edu>
2025-11-15 17:08:16 -0500

    Update arduino.ino

--- untitled folder 4/arduino.ino
+++ untitled folder 4/arduino.ino
@@ -1,22 +1,44 @@
 const int piezo1 = A0;
-const int piezo2 = A1;
 int piezo1Val;
+const int piezo2 = A1;
 int piezo2Val;
+const int switchPin = 2;
+int switchVal;
+#include <SimpleWebSerial.h>
+
+// WebSerial can be any variable name you want.
+SimpleWebSerial WebSerial;
 
 void setup() {
+  
   // put your setup code here, to run once:
   Serial.begin(9600);
+  WebSerial.send("event-from-arduino", switchVal);
+  WebSerial.on("event-to-arduino", eventCallback);
+   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  piezo1Val = analogRead(piezo1);
-  piezo2Val = analogRead(piezo2);
-//   Serial.print("Piezo 1: ");
-//   Serial.println(piezo1Val);
-//   Serial.print("Piezo 2: ");
-//   Serial.println(piezo2Val);
-Serial.write(piezo1);
+  //piezo1Val = analogRead(piezo1);
+  //piezo2Val = analogRead(piezo2);
+  switchVal = digitalRead(switchPin);
+  Serial.println(switchVal);
+  /*
+  Serial.print("piexo 1: ");
+  Serial.println(piezo1Val);
+  Serial.print("piexo 2: ");
+  Serial.println(piezo2Val);
+*/
+ WebSerial.send("event-from-arduino", switchVal);
 
-  delay(10);
+  WebSerial.check();
+  delay(100);
+ 
+}
+
+void eventCallback(JSONVar data){
+ 
+  WebSerial.send("event-from-arduino", switchVal);
+
 }
