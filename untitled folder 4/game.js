601255f8d88bcc5bc157daa6cc6d2abf30d29a51
Mariana Espinal <mce1571@g.rit.edu>
2025-11-17 19:08:54 -0500

    game over and timer

--- untitled folder 4/game.js
+++ untitled folder 4/game.js
@@ -10,34 +10,38 @@ let molePosition;
 let width = 1024;
 let retryButton;
 let menuButton;
+let gameIsOver = false;
+let spawner;
 /**increase the score */
 
 const increaseScore = () => {
- counter++;
- background(bgScene); // remove mole and score
- text(counter, 50, 10, 70, 80);
- molePresent = false;
- // gameLoop();
+    counter++;
+    background(bgScene); // remove mole and score
+    text(counter, 50, 10, 70, 80);
+    molePresent = false;
+    // gameLoop();
 }
 
 const decreaseScore = () => {
- counter-= 1;
- text(counter, 10, 10, 70, 80);
+    counter -= 1;
+    text(counter, 10, 10, 70, 80);
 }
 
 const decreaseTimer = () => {
     if (timer > 0) {
-        timer-=1;
+        timer -= 1;
     };
-     background(bgScene);
-      text(counter, 50, 10, 70, 80);
-     text('time:' + `${timer}`, 920, 10, 70, 80);
-     if(timer == 0) {
-     gameOver();
-     };
+    background(bgScene);
+    text(counter, 50, 10, 70, 80);
+    text('time:' + `${timer}`, 920, 10, 70, 80);
+    if (timer == 0) {
+        gameIsOver = true;
+        clearInterval(spawner);
+        gameOver();
+    };
 }
 //load game assets
-function preloadAssets()  {
+function preloadAssets() {
     moleSprite = loadImage("./assets/mole.png");
     bgScene = loadImage('./assets/bgmolee.png');
 };
@@ -47,18 +51,18 @@ If mole not in boundary, lose points or time */
 const buttonCheck = (buttonDirection) => {
 
     console.log("Buttons checked!");
- 
- if(molePosition < 500){
-    
+
+    if (molePosition < 500) {
+
         if (buttonDirection == 'rightButton') {
             increaseScore();
         }
     }
 
-    if (molePosition >= 500){
+    if (molePosition >= 500) {
         if (buttonDirection == 'leftButton') {
             increaseScore();
-        }   
+        }
     }
 
 };
@@ -68,121 +72,138 @@ const buttonCheck = (buttonDirection) => {
 
 //potentially could be turned nto spawn random objects function
 const spawnMole = () => {
- let position;
-//use a random to determine if the mole will spawn left or right.
-if(Math.random() < .5){
+    let position;
+    //use a random to determine if the mole will spawn left or right.
+    if (Math.random() < .5) {
         position = 500;
-        
-} else {
+
+    } else {
         position = 245;
-}
+    }
     image(moleSprite, position, 300);
     molePosition = position;
     molePresent = true;
 }
 
 
-function setup(){
-//assets go here
-preloadAssets();
-let canvas = createCanvas(1024, 512);
-textSize(25);
- gameStart();
+function setup() {
+    //assets go here
+    preloadAssets();
+    let canvas = createCanvas(1024, 512);
+
+    gameStart();
 
 }
 
 /** a black background with a start prompt */
-function gameStart ()  {
-  background(0);
+function gameStart() {
+    background(0);
+    if (retryButton || menuButton) {
+        retryButton.remove();
+        menuButton.remove();
+    }
+    //intro text
+    textSize(11)
+    introText = "WELCOME TO WHACKAMOLE";
+    fill(255);
+    text(introText, 50, 10, 70, 80);
 
-  //intro text
-  introText = "WELCOME TO WHACKAMOLE";
-  fill(255);
-  text(introText, 50, 10, 70, 80);
+    counter = 0;
 
-  counter = 0;
-
-  //start the game
- startButton = createButton('START');
-  startButton.position(width/3, 100);
-
-
-  startButton.mousePressed(gameLoop);
-}; 
-
-
-function gameLoop(){
-startButton.remove();
-// counter = 0;
-background(bgScene);
-// console.log(bgScene);
-
-text(counter, 50, 10, 70, 80);
-//canvas.clear();
-//background(bgScene);
+    //start the game
+    startButton = createButton('START');
+    startButton.position(width / 3, 100);
 
 
-//check if there's a mole on the screen
+    startButton.mousePressed(gameLoop);
+};
+
+
+function gameLoop() {
+    timer = 30;
+    textSize(25);
+    startButton.remove();
+
+
+       
+    
+    // counter = 0;
+    background(bgScene);
+    // console.log(bgScene);
+
+    text(counter, 50, 10, 70, 80);
+    //canvas.clear();
+    //background(bgScene);
+
+
+    //check if there's a mole on the screen
 
 
 
-setInterval(decreaseTimer, 1000);
-if (molePresent == false) {
-    //spawnMole
-    setInterval(spawnMole, 2000);
-}
-else if (molePresent == true){
-    buttonCheck();
-}
+    setInterval(decreaseTimer, 1000);
 
-//img(bg, position, 300);
+    if (molePresent == false) {
+        //spawnMole
+
+        spawner = setInterval(spawnMole, 2000);
+
+    }
+    else if (molePresent == true) {
+        buttonCheck();
+    }
+
+    //img(bg, position, 300);
 };
 
 /** shows high score and button that 
  * calls gameloop and a button for gameStart (titlescreen */
 function gameOver() {
-  background(0);
+    background(0);
 
-  //intro text
-  introText = "time's up!";
-  let scoreText = "your score: " + `${counter}`;
-  fill(255);
-  text(introText, 50, 10, 70, 80);
- text(scoreText, 50, 10, 70, 80);
+    //intro text
+    introText = "time's up!";
 
-  counter = 0;
+    fill(255);
+    text(introText, width / 2 - 20, 50, 70, 80);
+    text(scoreText, width / 2 + 100, 40, 70, 80);
 
-  //start the game
-  retryButton = createButton('Retry');
-  retryButton.position(width/3, 100);
+    counter = 0;
 
-
-  retryButton.mousePressed(gameLoop);
-
-   menuButton = createButton('Main menu');
-  menuButton.position(width/3, height - 30);
+    //start the game
+    retryButton = createButton('Retry');
+    retryButton.position(width / 3, 300);
 
 
-   menuButton.mousePressed(gameStart);
+    retryButton.mousePressed(gameLoop);
+
+    menuButton = createButton('Main menu');
+    menuButton.position(width / 3, height - 30);
+
+
+    menuButton.mousePressed(gameStart);
+
+    retryButton.mousePressed(gameLoop);
+     retryButton.remove();
+    menuButton.remove();
 }
 
-function mouseClicked(){
+function mouseClicked() {
 
-    if(molePresent == true){
+    if (molePresent == true) {
 
-        if(molePosition < 500){
-        
+        if (molePosition < 500) {
+
             if (mouseX < 500) {
                 increaseScore();
                 // console.log("click!");
             }
         }
 
-        if (molePosition >= 500){
+        if (molePosition >= 500) {
             if (mouseX >= 500) {
                 increaseScore();
                 // console.log("click!");
-            }   
+            }
         }
     }
 }
